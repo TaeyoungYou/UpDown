0459d3859e9bee487b18fcc3809db15bb647a19d
Taeyoung You <taeyoung.you12@gmail.com>
2024-02-01 10:34:20 -0500

    Version 2
    Coded clearScreen, loadingScreen
    for UI

--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,9 @@
+import java.io.IOException;
 import java.util.Random;
 import java.util.Scanner;
 
 public class Main{
-    public static void main(String[] args){
+    public static void main(String[] args) throws InterruptedException, IOException{
         Scanner scan = new Scanner(System.in);
         String name;
         boolean stop = false;
@@ -12,9 +13,14 @@ public class Main{
         Player user = new Player(name);
         UpDown n = new UpDown();
 
+        System.out.format("Hello, %s\n",user.getName());
         System.out.println("Game Start! Guess the number (1~100) - "+user.getName());
-        System.out.println("-----------------------------------------------------------");
+        loadingScreen();
+        clearScreen();
         while(!stop){
+
+            System.out.println("Game Start! Guess the number (1~100) - "+user.getName());
+            System.out.println("-----------------------------------------------------------");
             user.addAttempt();
             System.out.format("%d\n\n",user.getAttempt());
             System.out.print("Guess Number: ");
@@ -28,10 +34,26 @@ public class Main{
                 System.out.println("Down");
             }
             System.out.println("-----------------------------------------------------------");
+            Thread.sleep(1500);
+            clearScreen();
         }
+        clearScreen();
         System.out.println("Total:");
         System.out.format("\t User Name: %s\n",user.getName());
         System.out.format("\t User Attempt: %d\n",user.getAttempt());
+        Thread.sleep(1000);
+    }
+    public static void clearScreen() throws IOException, InterruptedException{
+        new ProcessBuilder("cmd","/c","cls").inheritIO().start().waitFor();
+    }
+    public static void loadingScreen() throws InterruptedException {
+        System.out.print("\tLoading");
+        for(int i=0;i<5;i++){
+            System.out.print("..");
+            Thread.sleep(500);
+        }
+        System.out.println("Start!");
+        Thread.sleep(1000);
     }
 }
 
